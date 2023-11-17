92d784420de025aa68b5fa0e9deb88e35aa55542
Tianyu Ren <ty.ren@outlook.com>
2023-11-17 21:51:18 +0000

    add initial distribution

--- scripts/train_pd_scripts/eval_lattice_rl.sh
+++ scripts/train_pd_scripts/eval_lattice_rl.sh
@@ -4,7 +4,7 @@ scenario='Eval'
 algo='DQN'
 exp="E25B32"
 env_dim=30
-dilemma_strength=1.05
+dilemma_strength=1.5
 seed_max=1
 
 
@@ -14,7 +14,7 @@ for seed in `seq ${seed_max}`;
 do
   echo "seed is ${seed}:"
   CUDA_VISIBLE_DEVICES=0 python ../train/train_lattice.py --env_dim ${env_dim} --algorithm_name ${algo} --log_interval 1 --num_env_steps 5000 \
-      --env_name ${env} --scenario_name ${scenario} --user_name 'tyren' --episode_length 10 --cuda --n_rollout_threads 2 --use_linear_lr_decay\
+      --env_name ${env} --scenario_name ${scenario} --user_name 'tyren' --episode_length 15 --cuda --n_rollout_threads 2 --use_linear_lr_decay\
       --mini_batch 32 --gradient_steps 1 --dilemma_strength ${dilemma_strength}  --target_update_interval 10000 --seed ${seed} --share_policy false \
       --experiment_name ${exp} --use_render --save_gif --lr 0.1 --video_interval 1 --use_linear_beta_decay --replay_scheme 'prioritized' --learning_starts 50 \
       --freq_type 'step' --train_freq 8 --prioritized_replay_alpha 0.6 --buffer_size 5000 --memory_alpha 0.6 --save_interval 0  \
--- scripts/train_pd_scripts/train_lattice_egt.sh
+++ scripts/train_pd_scripts/train_lattice_egt.sh
@@ -4,10 +4,10 @@ scenario='lattice_egt'
 algo='EGT'
 exp="check"
 env_dim=50
-dilemma_strength=1.1
+dilemma_strength=1.3
 
 echo "env is ${env}, scenario is ${scenario}, algorithm name is ${algo}, exp is ${exp}"
 
 CUDA_VISIBLE_DEVICES=0 python ../train/train_lattice.py --env_dim ${env_dim} --algorithm_name ${algo} --log_interval 50 --num_env_steps 5000 \
-    --env_name ${env}  --scenario_name ${scenario}  --user_name 'tyren' --use_wandb --episode_length 1 --dilemma_strength ${dilemma_strength} --n_rollout_threads 1 \
-    --rewards_pattern 'final' --K 0.1  --memory_alpha 0.9 --use_render  --video_interval 20 
+    --env_name ${env}  --scenario_name ${scenario}  --user_name 'tyren' --save_gif --episode_length 1 --dilemma_strength ${dilemma_strength} --n_rollout_threads 1 \
+    --rewards_pattern 'final' --K 0.1  --memory_alpha 0.9 --use_render  --video_interval 20 --init_distribution 'random'
