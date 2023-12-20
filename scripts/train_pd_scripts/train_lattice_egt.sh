c301bdb0dde72f4279e0b088c1181338aa578436
Tianyu Ren <ty.ren@outlook.com>
2023-12-20 14:11:06 +0000

    add

--- scripts/train_pd_scripts/train_lattice_egt.sh
+++ scripts/train_pd_scripts/train_lattice_egt.sh
@@ -3,11 +3,15 @@ env='Lattice'
 scenario='lattice_egt'
 algo='EGT'
 exp="check"
-env_dim=60
-dilemma_strength=1.05
+env_dim=30
 
 echo "env is ${env}, scenario is ${scenario}, algorithm name is ${algo}, exp is ${exp}"
 
-CUDA_VISIBLE_DEVICES=0 python ../train/train_lattice.py --env_dim ${env_dim} --algorithm_name ${algo} --log_interval 50 --num_env_steps 30000 --seed 2 \
-    --env_name ${env}  --scenario_name ${scenario}  --user_name 'tyren' --use_wandb --episode_length 1 --dilemma_strength ${dilemma_strength} --n_rollout_threads 1 \
-    --rewards_pattern 'final' --K 0.1  --memory_alpha 0.6 --use_render  --video_interval 50 --init_distribution 'random' --compare_reward_pattern 'none'
+# Loop to vary dilemma_strength from 1 to 3, incrementing by 0.1 each time
+for dilemma_strength in $(seq 1 0.1 1.2)
+do
+    echo "Running simulation with dilemma_strength: ${dilemma_strength}"
+    CUDA_VISIBLE_DEVICES=0 python ../train/train_lattice.py --env_dim ${env_dim} --algorithm_name ${algo} --log_interval 50 --num_env_steps 1000 --seed 1 \
+        --env_name ${env}  --scenario_name ${scenario}  --user_name 'tyren' --episode_length 1 --dilemma_strength ${dilemma_strength} --n_rollout_threads 1 \
+        --rewards_pattern 'final' --K 0.1  --memory_alpha 0.6 --use_render --use_wandb  --video_interval 50 --init_distribution 'random' --compare_reward_pattern 'none'
+done
