f3542cda59ce054dcf2ffff632e62fec8e76c9b1
Tianyu Ren <ty.ren@outlook.com>
2024-01-22 21:52:54 +0000

    update readme

--- scripts/train_pd_scripts/eval_lattice_combine.sh
+++ scripts/train_pd_scripts/eval_lattice_combine.sh
@@ -15,11 +15,11 @@ for seed in `seq ${seed_max}`;
 do
   echo "seed is ${seed}:"
   CUDA_VISIBLE_DEVICES=0 python ../train/train_lattice.py --env_dim ${env_dim} --eval_dim ${eval_dim} --algorithm_name ${algo} --log_interval 10 --num_env_steps 2000 \
-      --env_name ${env} --scenario_name ${scenario} --user_name 'tyren' --episode_length 1 --cuda --n_rollout_threads 1 --use_linear_lr_decay\
+      --env_name ${env} --scenario_name ${scenario} --user_name 'Anonymous' --episode_length 1 --cuda --n_rollout_threads 1 --use_linear_lr_decay\
       --mini_batch 32 --gradient_steps 1 --dilemma_strength ${dilemma_strength}  --target_update_interval 10000 --seed ${seed} --share_policy false \
       --experiment_name ${exp} --use_render --save_gif --lr 0.1 --video_interval 50 --use_linear_beta_growth --replay_scheme 'prioritized' --learning_starts 50 \
       --freq_type 'step' --train_freq 8 --prioritized_replay_alpha 0.6 --buffer_size 5000 --memory_alpha 0.6 --save_interval 0  \
       --max_files 2 --rewards_pattern 'final' --normalize_pattern 'none' --train_pattern 'together' \
-      --eval_mode --model_dir '../../results/Lattice/TestPC/DQN/E25B16/run_2023-11-18_19-41-27/models' 
+      --eval_mode --model_dir 'model_dir' 
 
 done
--- scripts/train_pd_scripts/train_lattice_egt.sh
+++ scripts/train_pd_scripts/train_lattice_egt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 env='Lattice'
-scenario='lattice_egt'
+scenario='EGT'
 algo='EGT'
 exp="check"
 env_dim=30
@@ -11,7 +11,7 @@ echo "env is ${env}, scenario is ${scenario}, algorithm name is ${algo}, exp is 
 for dilemma_strength in $(seq 1 0.1 1.2)
 do
     echo "Running simulation with dilemma_strength: ${dilemma_strength}"
-    CUDA_VISIBLE_DEVICES=0 python ../train/train_lattice.py --env_dim ${env_dim} --algorithm_name ${algo} --log_interval 50 --num_env_steps 1000 --seed 1 \
-        --env_name ${env}  --scenario_name ${scenario}  --user_name 'tyren' --episode_length 1 --dilemma_strength ${dilemma_strength} --n_rollout_threads 1 \
+    CUDA_VISIBLE_DEVICES=0 python ../train/train_lattice.py --env_dim ${env_dim} --algorithm_name ${algo} --log_interval 50 --num_env_steps 60000 --seed 1 \
+        --env_name ${env}  --scenario_name ${scenario}  --user_name 'Anonymous' --episode_length 1 --dilemma_strength ${dilemma_strength} --n_rollout_threads 1 \
         --rewards_pattern 'final' --K 0.1  --memory_alpha 0.6 --use_render --use_wandb  --video_interval 50 --init_distribution 'random' --compare_reward_pattern 'none'
 done
